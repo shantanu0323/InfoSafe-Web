0cc9f5c5d157285d5982126b721d839535c2129f
Shantanu Pramanik <srs.shaan@gmail.com>
2018-03-24 23:03:54 +0530

    login backend done

--- login/skeleton.js
+++ login/skeleton.js
@@ -1,28 +1,45 @@
 window.onload = execute();
 
 function execute() {
-    firebase.auth().signInWithEmailAndPassword("srs.shaan@gmail.com", "shantanu123").catch(function (error) {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            window.location = "../main.html";
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            alert(uid);
+            // ...
+        } else {
+            // User is signed out.
+            // ...
+        }
+    });
+}
+
+function loginUser() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         alert(errorMessage);
     });
-    
-    firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    alert(uid);
-    // ...
-  } else {
-    // User is signed out.
-    // ...
-  }
-});
+
+}
+
+function logout() {
+    firebase.auth().signOut().then(function () {
+        alert('Signed Out');
+    }, function (error) {
+        console.error('Sign Out Error', error);
+    });
+
 }
